c390848fc1c5aa762891ef187d6a0262dd383ff4
f.depalma <325587@via.dk>
2023-05-03 12:07:51 +0200

    add metadata

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,10 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="manifest"
+          href="/manifest.json"
+        />
         <meta name="theme-color" content="#000" />
         <title>Francesco De Palma</title>
       </Head>
